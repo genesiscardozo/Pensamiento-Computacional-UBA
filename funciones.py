618225cdc63c5d81bcab2c6647c981d0200349e6
Genesis <69064451+genesiscardozo@users.noreply.github.com>
2023-08-24 00:21:43 -0300

    Update and rename main.py to funciones.py

---
+++ funciones.py
@@ -0,0 +1,19 @@
+#Funciones: Empiezan en Def (Define)
+
+def saludar():
+  print("Hola!")
+  print("Espero que estés muy bien! :)")
+  
+saludar()
+
+def saludo_venecia():
+  print("Hola, Vene Bubu!")
+
+def saludo_mimi():
+  print("Hola, Mimi!")
+  
+saludo_venecia()
+saludo_mimi()
+
+def naming(nombre):
+  print("Hola " + nombre + "! Espero que estés muy bien! :)") 
